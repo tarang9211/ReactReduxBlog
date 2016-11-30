e65e91edfd139ea33376cba3406bc16d451041f4
Tarang Hirani <tarang9211@gmail.com>
2016-12-01 02:50:30 +0530

    refactored posts_index component to be a react component instead of a functional component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default () => {
-  return (
-    <div>
-      List of blog posts
-    </div>
-  );
+class PostsIndex extends Component {
+  componentWillMount() {
+    
+  }
+
+  render() {
+    return (
+      <div>
+        Lists of blog posts
+      </div>
+    );
+  }
 }
+
+export default PostsIndex;
