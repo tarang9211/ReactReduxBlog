599c742c76996156f4edc976640c57c88fd2f2e9
Tarang Hirani <tarang9211@gmail.com>
2016-12-02 02:10:06 +0530

    added basic form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -7,12 +7,16 @@ class PostsNew extends Component {
     //extracting values from this.props
     const { fields: {title, categories, content}, handleSubmit} = this.props;
 
+    //handleSubmit is created by redux-form and submits the form
     return (
       <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create a new post</h3>
         <div className='form-group'>
           <label>Title</label>
           <input type='text' className='form-control' {...title}/>
+          <div className='text-help'>
+            {title.touched ? title.error : ''}
+          </div>
         </div>
 
         <div className='form-group'>
@@ -31,6 +35,21 @@ class PostsNew extends Component {
   }
 }
 
+/*
+validate method must return an errors object.
+the properties of the errors object must correspond to the form fields
+*/
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Please enter a username';
+  }
+
+
+  return errors;
+}
+
 //connect: first arg is mapStateToProps, second arg is mapDispatchToProps
 //redux-form: first arg is form config, second arg is mapStateToProps
 //third arg is mapDispatchToProps
@@ -39,5 +58,6 @@ class PostsNew extends Component {
 //reduxForm acts like connect
 export default reduxForm({
   form: 'PostsNewForm', //form name must be unique
-  fields: ['title', 'categories', 'content']
+  fields: ['title', 'categories', 'content'],
+  validate
 }, null, {createPost})(PostsNew);
