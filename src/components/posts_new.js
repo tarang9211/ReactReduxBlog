4d62c0f3d4436a57e73dfcdf2a9694d6d68538d1
Tarang Hirani <tarang9211@gmail.com>
2016-12-02 01:37:24 +0530

    passing input controls to redux forms

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,22 +3,25 @@ import {reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
   render() {
+    //extracting values from this.props
+    const { fields: {title, categories, content}, handleSubmit} = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <h3>Create a new post</h3>
         <div className='form-group'>
           <label>Title</label>
-          <input type='text' className='form-control'/>
+          <input type='text' className='form-control' {...title}/>
         </div>
 
         <div className='form-group'>
           <label>Categories</label>
-          <input type='text' className='form-control' />
+          <input type='text' className='form-control' {...categories}/>
         </div>
 
         <div className='form-group'>
           <label>Content</label>
-          <textarea type='text' className='form-control' />
+          <textarea type='text' className='form-control' {...content}/>
         </div>
 
         <button type='submit' className='btn btn-primary'>Submit</button>
@@ -27,6 +30,7 @@ class PostsNew extends Component {
   }
 }
 
+//this injects into component props
 export default reduxForm({
   form: 'PostsNewForm', //form name must be unique
   fields: ['title', 'categories', 'content']
