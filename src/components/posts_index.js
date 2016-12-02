88b64437df3c257525b46e48564ad319eac2932a
Tarang Hirani <tarang9211@gmail.com>
2016-12-02 16:16:29 +0530

    feature implemented to delete from list

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {fetchPosts} from '../actions/index';
+import {fetchPosts, deletePost} from '../actions/index';
 import {Link} from 'react-router';
 
 //this class is a **container**
@@ -9,11 +9,18 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  handleDelete(id) {
+    this.props.deletePost(id)
+      .then(() => {
+        this.props.fetchPosts();
+      })
+  }
+
   renderPosts() {
     return this.props.posts.map((post) => {
       return (
         <li className='list-group-item padding-bottom' key={post.id}>
-          <button className='btn btn-danger pull-xs-right'>Delete</button>
+          <button className='btn btn-danger pull-xs-right' onClick={this.handleDelete.bind(this, post.id)}>Delete</button>
           <Link to={'/posts/' + post.id}>
             <span className='pull-xs-right margin-right'>{post.categories}</span>
             <strong>{post.title}</strong>
@@ -42,4 +49,7 @@ function mapStateToProps(state) {
   return { posts: state.posts.all };
 }
 //fetchPosts action is available as this.props.fetchPosts
-export default connect(mapStateToProps, { fetchPosts: fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, {
+  fetchPosts: fetchPosts,
+  deletePost: deletePost
+})(PostsIndex);
