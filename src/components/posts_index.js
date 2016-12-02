ad6df641505757bb77da43f4f4768b24cc4682af
Tarang Hirani <tarang9211@gmail.com>
2016-12-02 15:20:28 +0530

    fixing eslint config

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
 import {fetchPosts} from '../actions/index';
 import {Link} from 'react-router';
 
